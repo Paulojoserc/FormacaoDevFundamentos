8dcb4a591798d145f378aabb1fadcb5634f45de8
Paulojoserc <pjrc83@gmail.com>
2025-01-08 00:25:07 -0300

    Componente Logo

--- ordem-livros/src/routes/+layout.svelte
+++ ordem-livros/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+  import Logo from '../components/template/Logo.svelte';
 	let { children } = $props();
 </script>
 
+<div class="flex flex-col justify-center items-center bg-black text-white min-h-screen">
+
+	<Logo/>
+
 {@render children()}
+</div>
+
+
--- ordem-livros/src/routes/+page.svelte
+++ ordem-livros/src/routes/+page.svelte
@@ -1,16 +1,2 @@
-<script>
-  import Caixa from "../components/Caixa.svelte";
-
-</script>
-<div class="flex flex-col justify-center items-center gap-5
-bg-black text-white min-h-screen">
-
 <h1 class="text-3x1 font-bold">Ordem dos livros</h1>
 <a href="/autores">Autores</a>
-</div>
-
-<Caixa />
-<Caixa />
-<Caixa />
-<Caixa />
-<Caixa />
